5770db3ae755ab72b38be3eaff4b97ad95ac34aa
BriceInCode <brice.nanyang1@gmail.com>
2025-02-22 11:05:05 +0100

    Ajustement de la documentation des codes source

--- src/model/Coordinate.java
+++ src/model/Coordinate.java
@@ -2,7 +2,10 @@ package model;
 
 import java.util.Objects;
 
-/** * Représente une coordonnée dans la grille du jeu. ** */
+/**
+ * Représente une coordonnée dans la grille du jeu. Chaque coordonnée est composée de deux valeurs :
+ * une abscisse (x) et une ordonnée (y).
+ */
 public class Coordinate {
     private int x;
     private int y;
@@ -29,17 +32,29 @@ public class Coordinate {
         this(old.x + x, old.y + y);
     }
 
-    /** * Retourne la coordonnée en abscisse.  */
+    /**
+     * Retourne la coordonnée en abscisse.
+     *
+     * @return La coordonnée en abscisse (x).
+     */
     public int getX() {
         return x;
     }
 
-    /** * Retourne la coordonnée en abscisse.  */
+    /**
+     * Retourne la coordonnée en ordonnée.
+     *
+     * @return La coordonnée en ordonnée (y).
+     */
     public int getY() {
         return y;
     }
 
-    /** * Retourne la coordonnée en ordonnée.  */
+    /**
+     * Retourne le code de hachage de la coordonnée basé sur les valeurs de x et y.
+     *
+     * @return Le code de hachage de la coordonnée.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(x, y);
@@ -59,7 +74,11 @@ public class Coordinate {
         return x == that.x && y == that.y;
     }
 
-    /** * Retourne une représentation textuelle de la coordonnée.  */
+    /**
+     * Retourne une représentation textuelle de la coordonnée sous la forme (x/y).
+     *
+     * @return La représentation sous forme de chaîne de caractères de la coordonnée.
+     */
     @Override
     public String toString() {
         return String.format("(%d/%d)", x, y);
